4f5d13b362485dc4b229d81b581c8fcf7295c6a2
mbaseman1918 <39381570+mbaseman1918@users.noreply.github.com>
2018-07-13 15:58:41 -0700

    Fibonacci able to count to 207260th index

--- fibonacci.py
+++ fibonacci.py
@@ -7,3 +7,11 @@ def fib(n):
         return fib(n-1) + fib(n-2)
 
 print(fib(9))
+
+def list_fib(n):
+    fib_num = [0,1]
+    for i in range(n-1):
+        fib_num.append(fib_num[-1] + fib_num[-2])
+    return fib_num[-1]
+
+print(list_fib(207260))
