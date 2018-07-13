5871566f04f09f013ed86c562f5b6fc7d5555815
mbaseman1918 <39381570+mbaseman1918@users.noreply.github.com>
2018-07-13 15:10:34 -0700

    work in progress contactlist

--- contactlist.py
+++ contactlist.py
@@ -1,9 +1,3 @@
-class person(object):
-    def __init__(self, name, fav_fruit, fav_color):
-        self.name = name
-        self.fav_fruit = fav_fruit
-        self.fav_color = fav_color
-
 with open("c_list.csv", "r") as f:
     lines = f.read().split("\n")
     lines = lines[:-1]
@@ -12,14 +6,36 @@ with open("c_list.csv", "r") as f:
     print(lines)
     # book = f.read()
     # print(book)
-contacts = {}
-for object in lines[1:]:
-    print(object)
-    index = object.split(",")[0]
-    counter = 0
-    contacts[index] = {}
-    for item in object.split(","):
-        print(item)
-        contacts[index][headings[counter]] = item
-        counter += 1
+    contacts = {}
+    for object in lines[1:]:
+        print(object)
+        index = object.split(",")[0]
+        counter = 0
+        contacts[index] = {}
+        for item in object.split(","):
+            print(item)
+            contacts[index][headings[counter]] = item
+            counter += 1
+
+class person():
+    def __init__(self, name, fav_fruit, fav_color):
+        self.name = name
+        self.fav_fruit = fav_fruit
+        self.fav_color = fav_color
+
+    def create(self, contact_dict, headings_list):
+        contact_dict[self.name] = {headings[0]: self.name, headings[1]: self.fav_fruit, headings[2]: self.fav_color}
+
+    def retrieve():
+        pass
+
+    def update():
+        pass
+
+    def delete():
+        pass
+
+Frank = person("Frank", "bananas", "blue")
+Frank.create(contacts, headings)
+
 print(contacts)
