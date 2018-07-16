6717a52da13ed067d674bf2ea45ba2e0827df6ce
mbaseman1918 <39381570+mbaseman1918@users.noreply.github.com>
2018-07-16 15:34:30 -0700

    updated contactlist and created initial HTML and CSS docs

--- contactlist.py
+++ contactlist.py
@@ -1,41 +1,46 @@
-with open("c_list.csv", "r") as f:
-    lines = f.read().split("\n")
-    lines = lines[:-1]
-    headings = lines[0].split(",")
-    print(headings)
-    print(lines)
-    # book = f.read()
-    # print(book)
-    contacts = {}
-    for object in lines[1:]:
-        print(object)
-        index = object.split(",")[0]
-        counter = 0
-        contacts[index] = {}
-        for item in object.split(","):
-            print(item)
-            contacts[index][headings[counter]] = item
-            counter += 1
 
-class person():
-    def __init__(self, name, fav_fruit, fav_color):
-        self.name = name
-        self.fav_fruit = fav_fruit
-        self.fav_color = fav_color
+class contactlist():
+    def __init__(self, contact_list):
 
-    def create(self, contact_dict, headings_list):
-        contact_dict[self.name] = {headings[0]: self.name, headings[1]: self.fav_fruit, headings[2]: self.fav_color}
+        with open(contact_list, "r") as f:
+            lines = f.read().split("\n")
+            lines = lines[:-1]
+            headings = lines[0].split(",")
+            print(headings)
+            print(lines)
+            # book = f.read()
+            # print(book)
+            contacts = {}
+            for object in lines[1:]:
+                print(object)
+                index = object.split(",")[0]
+                counter = 0
+                contacts[index] = {}
+                for item in object.split(","):
+                    print(item)
+                    contacts[index][headings[counter]] = item
+                    counter += 1
+        self.contacts = contacts
+        self.headings = headings
+
+    def create(self):
+        name = input("What is the name of the person? ")
+        fav_fruit = input("What is this person's favorite fruit? ")
+        fav_color = input("What is this person's favorite color? ")
+        self.contacts[name] = {self.headings[0]: name, self.headings[1]: fav_fruit, self.headings[2]: fav_color}
+        return f"\nThe following information has been added to the contact list:\n {self.contacts[name]}\n"
 
     def retrieve():
-        pass
+        name = input("What is the name of the person? ")
+
 
     def update():
-        pass
+        name = input("What is the name of the person? ")
+        trait = input("What is the trait you would like to update? ")
 
     def delete():
-        pass
+        name = input("What is the name of the contact you would like to delete? ")
 
-Frank = person("Frank", "bananas", "blue")
-Frank.create(contacts, headings)
-
-print(contacts)
+contact_book = contactlist("c_list.csv")
+print(contact_book.create())
+print(contact_book.contacts)
